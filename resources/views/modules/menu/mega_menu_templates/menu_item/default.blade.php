696ba0bc5f78f2346480e1e27dcdad8d44de51ca
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 15:57:19 +0200

    Update default.blade.php

--- resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
+++ resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
@@ -10,7 +10,7 @@ description: Default Menu Item skin
 
 */
 ?>
-<div id="mega-menu-full-dropdown" class="hide group-hover:block bg-gray-50 md:bg-white left-0 w-full absolute mt-4 border-gray-200 shadow-xs  border-y dark:bg-gray-800 dark:border-gray-600">
+<div id="mega-menu-full-dropdown" class="hidden group-hover:block bg-gray-50 md:bg-white left-0 w-full absolute mt-4 border-gray-200 shadow-xs  border-y dark:bg-gray-800 dark:border-gray-600">
     <div  rel="{{$menu_item_id}}" field="mega_menu_item" class="edit grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
 
         <div>
