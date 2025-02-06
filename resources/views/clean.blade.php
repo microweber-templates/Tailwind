e8c9c3e1cdfeeff7fe43d0b674cb379c0bb196d4
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 11:49:44 +0200

    update

---
+++ resources/views/clean.blade.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+
+type: layout
+content_type: static
+name: Clean
+position: 6
+description: Clean
+
+*/
+
+?>
+@extends('templates.tailwind::layouts.master')
+
+@section('content')
+
+
+<div>
+
+    Clean
+
+</div>
+
+
+
+@endsection
