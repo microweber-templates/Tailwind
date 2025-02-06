23925a26837074245243f359ec6d600b8d5b1b26
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 13:57:55 +0200

    update

---
+++ resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+
+type: layout
+
+name: Default
+
+description: Default Menu Item skin
+
+*/
+
+?>
+<div class="edit">
+    Here is your mega menu content.. TAILWINDDDD
+</div>
+
+
+
+<div id="mega-menu-full-dropdown" class=" mt-1 border-gray-200 shadow-xs bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+    <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
+        <ul>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Online Stores</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Segmentation</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Marketing CRM</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Online Stores</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Segmentation</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <div class="font-semibold">Marketing CRM</div>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
