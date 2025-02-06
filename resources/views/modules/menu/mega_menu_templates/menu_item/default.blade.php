ff679db724c4dd9dbb39b083939b5c5c95b7d718
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 15:49:24 +0200

    update

--- resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
+++ resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
@@ -10,47 +10,16 @@ description: Default Menu Item skin
 
 */
 ?>
-<div id="mega-menu-full-dropdown" rel="{{$menu_item_id}}" field="mega_menu_item" class="edit hidden group-hover:block bg-gray-50 md:bg-white left-0 w-full absolute mt-4 border-gray-200 shadow-xs  border-y dark:bg-gray-800 dark:border-gray-600">
-    <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
-        <ul>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Online Stores</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Segmentation</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Marketing CRM</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Online Stores</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Segmentation</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <div class="font-semibold">Marketing CRM</div>
-                    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-                </a>
-            </li>
-        </ul>
+<div id="mega-menu-full-dropdown" class="  group-hover:block bg-gray-50 md:bg-white left-0 w-full absolute mt-4 border-gray-200 shadow-xs  border-y dark:bg-gray-800 dark:border-gray-600">
+    <div  rel="{{$menu_item_id}}" field="mega_menu_item" class="edit grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
+
+        <div>
+            <module type="menu" template="mega_menu_item_default" name="mega_menu_item_{{$menu_item_id}}" />
+        </div>
+
+        <div>
+            <module type="maps" />
+        </div>
+
     </div>
 </div>
---
+++ resources/views/modules/menu/templates/mega_menu_item_default.blade.php
@@ -0,0 +1,4 @@
+<a href="#" class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+    <div class="font-semibold">Online Stores</div>
+    <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+</a>
