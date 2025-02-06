74bf4e28f962b5e69c61c4e1a983c5263bf0910f
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 13:56:57 +0200

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -31,7 +31,7 @@
             </ul>
         </div>
     </div>
-    <div id="mega-menu-full-dropdown" class="hidden mt-1 border-gray-200 shadow-xs bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+    <div id="mega-menu-full-dropdown" class=" mt-1 border-gray-200 shadow-xs bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
         <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
             <ul>
                 <li>
