1439b0960b18a8bb00c63a6f739c85ec050f1698
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-06 15:13:24 +0200

    update

--- resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
+++ resources/views/modules/menu/mega_menu_templates/menu_item/default.blade.php
@@ -11,13 +11,8 @@ description: Default Menu Item skin
 */
 
 ?>
-<div class="edit">
-    Here is your mega menu content.. TAILWINDDDD
-</div>
 
-
-
-<div id="mega-menu-full-dropdown" class=" mt-1 border-gray-200 shadow-xs bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+<div id="mega-menu-full-dropdown" class="hidden group-hover:block bg-gray-50 md:bg-white left-0 w-full absolute mt-4 border-gray-200 shadow-xs  border-y dark:bg-gray-800 dark:border-gray-600">
     <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
         <ul>
             <li>
---
+++ resources/views/modules/menu/templates/header.blade.php
@@ -0,0 +1,20 @@
+{{--
+type: layout
+name: Header Menu
+description: Header menu template
+--}}
+
+@php
+$menu_filter['ul_class'] = 'flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700';
+$menu_filter['li_class'] = 'group';
+$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="bg-red-500 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700 {active_class}" href="{url}">{title}</a>';
+$mt = menu_tree($menu_filter);
+@endphp
+
+<div class="items-center justify-between font-medium w-full md:flex md:w-auto md:order-1">
+    @if($mt)
+        {!! $mt !!}
+    @else
+        {!! lnotif("There are no items in the menu <b>" . $menu_name . '</b>') !!}
+    @endif
+</div>
